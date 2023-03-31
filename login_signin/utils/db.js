9f2bcff6c3aedb18ef502bb2846395c4b323d2b7
Haywhyoh <adefeyisayo998@gmail.com>
2023-03-31 10:23:44 +0100

    completed the db client class

---
+++ login_signin/utils/db.js
@@ -0,0 +1,25 @@
+const mongodb = require('mongodb');
+
+class DBClient {
+    constructor() {
+        const uri = "mongodb+srv://adefeyisayo998:Mydreams@cluster0.nujcd7c.mongodb.net/?retryWrites=true&w=majority";
+        this.client = new mongodb.MongoClient(uri,  { useUnifiedTopology: true });
+        this.client.connect();
+    }
+
+    isAlive() {
+        return this.client.isConnected();
+    }
+
+    async nbUsers() {
+        return this.client.db('userdb').collection('users').countDocuments({});
+    }
+
+ 
+    async userCollection() {
+        return this.client.db('userdb').collections('users');
+    }
+}
+
+const dbClient = new DBClient();
+module.exports = dbClient;
