407ea409213a1ced63455b4fa9e33dc8fd7bf1f8
haywhyoh <adefeyisayo998@gmail.com>
2023-04-05 12:01:36 +0000

    removed the newUrl pasrse

--- node_auth/db.js
+++ node_auth/db.js
@@ -3,7 +3,6 @@ const mongoose = require('mongoose');
 const localDB = `mongodb://localhost:27017/role_auth`
 const connectDb = async () => {
   await mongoose.connect(localDB, {
-    useNewUrlParse: true,
     useUnifiedTopology: true
   })
   console.log('MongoDb Connected');
