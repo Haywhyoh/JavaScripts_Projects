165471811e5f8860e6e974f9ea3de26485502656
haywhyoh <adefeyisayo998@gmail.com>
2023-04-05 10:48:11 +0000

    created user schema with username, pssword and rol

---
+++ node_auth/user.js
@@ -0,0 +1,24 @@
+const Mongoose = require('mongoose');
+
+const userSchema = new Mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+
+  password: {
+    type: String,
+    required: true,
+    minLength: 6,
+  },
+
+  role: {
+    type: String,
+    default: Basic,
+    required: true
+  }
+})
+
+const User = Mongoose.model("user", userSchema);
+module.exports = User;
