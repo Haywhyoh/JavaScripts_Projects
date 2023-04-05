4fba9cc0357dbacb7b29872de0960c1523536aa3
haywhyoh <adefeyisayo998@gmail.com>
2023-04-05 17:08:47 +0100

    created the user login route

--- node_auth/auth/auth.js
+++ node_auth/auth/auth.js
@@ -1,13 +1,13 @@
 const user = require('../user');
-exports.register = async (req, res, next) => {
 
+exports.register = async (req, res, next) => {
   const { username, password } = req.body;
 
-  // if (password.length < 6) {
-  //   return res.status(400).json({
-  //     error: "Password too short"
-  //   })
-  // }
+  if (password.length < 6) {
+    return res.status(400).json({
+      error: 'Password too short'
+    });
+  }
   try {
     await user.create({
       username,
@@ -15,13 +15,44 @@ exports.register = async (req, res, next) => {
     }).then(user => {
       res.status(200).json({
         user,
-        message: "User created",
-      })
-    })
-  } catch(err) {
+        message: 'User created'
+      });
+    });
+  } catch (err) {
     res.status(401).json({
       error: err.message,
-      message: "User not created"
-    })
+      message: 'User not created'
+    });
   }
-}
+};
+
+exports.register = async (req, res, next) => {
+  const { username, password } = req.body;
+
+  if (!username || !password) {
+    return res.status(403).json({
+      error: 'Username and Password missed'
+    });
+  }
+
+  try {
+    const userDetails = await user.findOne({ username, password });
+
+    if (!userDetails) {
+      res.status(401).json({
+        message: 'Login not successful',
+        error: 'User not Found'
+      });
+    }
+
+    res.status(200).json({
+      message: 'User found',
+      userDetails
+    });
+  } catch (err) {
+    res.status(400).json({
+      message: 'An error occured',
+      error: err.message
+    });
+  }
+};
--- node_auth/server.js
+++ node_auth/server.js
@@ -1,18 +1,18 @@
 const express = require('express');
 const connectDb = require('./db');
-const register = require('./auth/routes')
+const router = require('./auth/routes');
 const app = express();
 const PORT = 5000;
 app.use(express.json());
 
-//connected database
+//  connected database
 connectDb();
 
-app.use("/api/auth", register)
+app.use('/api/auth', router);
 
 const server = app.listen(PORT, console.log(`App is running at port ${PORT}`))
 
-process.on("unhandledRejection", err => {
+process.on('unhandledRejection', err => {
   console.log(`There is a error: ${err.message}`);
   server.close(() => process.exit(1));
-})
+});
